76220b77558d10b2083c898f23d6dddb1888e0a1
Ian Becker <ian@beckr.dev>
2024-01-05 13:12:56 -0500

    fix: removes non-existent fields

--- src/types/reddit/Subreddit.ts
+++ src/types/reddit/Subreddit.ts
@@ -3,17 +3,17 @@ import { parsePost } from "./Post";
 
 export default interface Subreddit {
     name: string,
-    title: string,
-    description: string,
+    // title: string,
+    // description: string,
     posts: Post[]
 }
 
-export async function parseSubreddit(subreddit: any): Promise<Subreddit> {
+export async function parseSubreddit(name: string, subreddit: any): Promise<Subreddit> {
     const parsedSubreddit: Subreddit = {
-        name: subreddit.data.name,
-        title: subreddit.data.title,
-        description: subreddit.data.public_description,
-        posts: await Promise.all(subreddit.data.children.map(parsePost))
+        name,
+        // title: subreddit?.data?.title,
+        // description: subreddit?.data?.public_description,
+        posts: await Promise.all(subreddit?.data?.children?.map(parsePost))
     };
 
     return parsedSubreddit;

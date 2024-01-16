58a0b7b7343b24d07f94c038bd531bcecf1425aa
Ian Becker <ian@beckr.dev>
2024-01-16 17:57:24 -0500

    feat(hn): adds navbar and preloads
    ---
    
    still needs work because it was originally created with just reddit in mind, but the themepicker works and looks nice

--- src/routes/hn/+layout.svelte
+++ src/routes/hn/+layout.svelte
@@ -1,16 +1,53 @@
 <script>
   import "../../app.css";
+  import Navbar from "../../components/Navbar.svelte";
+  const themes = [
+    "light",
+    "dark",
+    "synthwave",
+    "retro",
+    "cyberpunk",
+    "forest",
+    "aqua",
+    "lofi",
+    "fantasy",
+    "wireframe",
+    "black",
+    "dracula",
+    "business",
+    "acid",
+    "lemonade",
+    "night",
+    "coffee",
+    "dim",
+    "nord",
+    "sunset",
+  ];
+  let theme = "dark";
 </script>
 
-<div data-theme="dark" class="navbar sticky top-0 z-50">
+<div class="sticky top-0 z-50 bg-secondary">
+  <Navbar {theme} {themes} currentSubreddit="top" />
   <div class="tooltip tooltip-bottom" data-tip="Top">
-    <a class="btn btn-ghost text-xl" href="/hn">Home</a>
+    <a class="btn btn-ghost text-xl" href="/hn" data-sveltekit-preload-data
+      >Home</a
+    >
   </div>
-  <a class="btn btn-ghost text-xl" href="/hn/new">New</a>
-  <a class="btn btn-ghost text-xl" href="/hn/best">Best</a>
-  <a class="btn btn-ghost text-xl" href="/hn/ask">Ask</a>
-  <a class="btn btn-ghost text-xl" href="/hn/show">Show</a>
-  <a class="btn btn-ghost text-xl" href="/hn/jobs">Jobs</a>
+  <a class="btn btn-ghost text-xl" href="/hn/new" data-sveltekit-preload-data
+    >New</a
+  >
+  <a class="btn btn-ghost text-xl" href="/hn/best" data-sveltekit-preload-data
+    >Best</a
+  >
+  <a class="btn btn-ghost text-xl" href="/hn/ask" data-sveltekit-preload-data
+    >Ask</a
+  >
+  <a class="btn btn-ghost text-xl" href="/hn/show" data-sveltekit-preload-data
+    >Show</a
+  >
+  <a class="btn btn-ghost text-xl" href="/hn/jobs" data-sveltekit-preload-data
+    >Jobs</a
+  >
 </div>
 
 <slot />

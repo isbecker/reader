e70341c94119b49a03bc3a0d36e0f32d86a5b09d
Ian Becker <ian@beckr.dev>
2024-01-15 23:45:43 -0500

    feat(hn): improves accessibility

--- src/components/hn/StoryCard.svelte
+++ src/components/hn/StoryCard.svelte
@@ -2,23 +2,14 @@
   import type Story from "../../types/hn/Story";
 
   export let story: Story;
-
 </script>
 
 <div>
   <!-- svelte-ignore a11y-no-static-element-interactions -->
   <!-- svelte-ignore a11y-click-events-have-key-events -->
-  <div
-    class="card card-bordered hover:bg-base-200"
-    on:click={() => {
-      const newWindow = window.open(story.url, "_blank");
-      if (newWindow) {
-        newWindow.focus();
-      }
-    }}
-  >
+  <div class="card card-bordered hover:bg-base-200">
     <div class="card-body">
-      <div class="card-title flex-col place-items-start">
+      <a class="card-title flex-col place-items-start" href={story.url}>
         <p>{story.title}</p>
         <div class="join gap-1">
           <p class="text-xs">{story.score} points</p>
@@ -27,15 +18,11 @@
             at {new Date(story.time * 1000).toLocaleString()}
           </p>
         </div>
-      </div>
+      </a>
       <div class="card-actions justify-end">
-        <button
-          class="btn btn-primary"
-          on:click={(event) => {
-            event.stopPropagation();
-            window.location.href = `/hn/item/${story.id}`;
-          }}>{story.descendants ?? 0} Comments</button
-        >
+        <a class="btn btn-primary" href="/hn/item/{story.id}">
+          {story.descendants ?? 0} Comments
+        </a>
       </div>
     </div>
   </div>

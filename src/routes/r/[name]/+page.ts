bceeae468b9283fcd277283b2eb06a1e97623a66
Ian Becker <ian@beckr.dev>
2024-01-18 15:19:22 -0500

    fix(reddit): adds timestamp query param

--- src/routes/r/[name]/+page.ts
+++ src/routes/r/[name]/+page.ts
@@ -1,5 +1,6 @@
 import type Subreddit from "../../../types/reddit/Subreddit";
 import { parseSubreddit } from "../../../types/reddit/Subreddit";
+import moment from "moment";
 
 export async function load({ fetch, params }) {
 
@@ -19,7 +20,7 @@ async function fetchSubreddit(subreddit: string,
 
     while (retryCount < maxRetries) {
         try {
-            const response = await fetch(`https://corsproxy.io/?${encodeURIComponent(`https://www.reddit.com/r/${subreddit}/hot.json`)}`);
+            const response = await fetch(`https://corsproxy.io/?${encodeURIComponent(`https://www.reddit.com/r/${subreddit}/hot.json?t=${moment().unix()}`)}`);
             // const response = await fetch(`/api/reddit/${subreddit}`);
             if (response.ok) {
                 return await response.json();

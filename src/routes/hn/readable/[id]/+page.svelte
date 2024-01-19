2c1f7282bd05e22c45d0d8465d6a91cbeb7c506f
Ian Becker <ian@beckr.dev>
2024-01-19 13:20:40 -0500

    feat(hn): adds comment count in reader view

--- src/routes/hn/readable/[id]/+page.svelte
+++ src/routes/hn/readable/[id]/+page.svelte
@@ -11,7 +11,9 @@
 <div class="flex flex-col mx-auto place-items-center pt-2">
   <div class="flex flex-row join gap-10">
     <div class="tooltip tooltip-bottom" data-tip="Back to HN">
-      <a class="btn btn-primary" href="/hn/item/{story.id}">Comments</a>
+      <a class="btn btn-primary" href="/hn/item/{story.id}"
+        >{story.descendants ?? 0} Comments</a
+      >
     </div>
     <article
       class="grow self-center text-base md:text-lg lg:text-3xl xl:text-5xl"

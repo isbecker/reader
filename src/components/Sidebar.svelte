57cb703b2500ad5334d20b2c3b9f5460ee063197
Ian Becker <ian@beckr.dev>
2024-01-19 17:12:38 -0500

    fix(style): adjusts sidebar sizing and ux + adds close button

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -15,34 +15,76 @@
 
         newSub = "";
     }
+    const links = [
+        { name: "Top", url: "/hn/top" },
+        { name: "Best", url: "/hn/best" },
+        { name: "New", url: "/hn/new" },
+        { name: "Ask", url: "/hn/ask" },
+        { name: "Show", url: "/hn/show" },
+        { name: "Jobs", url: "/hn/jobs" },
+    ];
 </script>
 
-<div class="flex flex-col h-screen bg-base-100 w-fit">
-    <ul class="menu bg-base-200 rounded-box">
+<div class="flex flex-col h-screen bg-base-100 w-min max-w-full">
+    <div>
+        <label
+            for="side-drawer"
+            aria-label="open sidebar"
+            class="btn btn-square btn-ghost"
+        >
+            <svg
+                class="swap-on fill-current"
+                xmlns="http://www.w3.org/2000/svg"
+                width="32"
+                height="32"
+                viewBox="0 0 512 512"
+                ><polygon
+                    points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"
+                /></svg
+            >
+        </label>
+    </div>
+    <ul class="menu bg-base-100 rounded-box">
         <li>
             <details open>
-                <summary><a href="/hn/top">Hacker News</a></summary>
-                <ul>
-                    <li><a href="/hn/top">Top</a></li>
-                    <li><a href="/hn/best">Best</a></li>
-                    <li><a href="/hn/new">New</a></li>
-                    <li><a href="/hn/ask">Ask</a></li>
-                    <li><a href="/hn/show">Show</a></li>
-                    <li><a href="/hn/jobs">Jobs</a></li>
+                <summary
+                    ><a class="btn btn-ghost self-stretch" href="/hn/top"
+                        >Hacker News</a
+                    ></summary
+                >
+
+                <ul class="menu bg-base-100 rounded-box">
+                    {#each links as link}
+                        <li>
+                            <a
+                                class="btn btn-ghost self-stretch"
+                                href={link.url}>{link.name}</a
+                            >
+                        </li>
+                    {/each}
                 </ul>
             </details>
         </li>
         <li>
             <details open>
-                <summary><a href="/r/all">Reddit</a></summary>
+                <summary
+                    ><a class="btn btn-ghost self-stretch" href="/r/all"
+                        >Reddit</a
+                    ></summary
+                >
                 <ul>
                     {#each $subscriptions as subreddit}
-                        <li><a href={subreddit.url}>r/{subreddit.name}</a></li>
+                        <li>
+                            <a class="btn btn-ghost" href={subreddit.url}
+                                >r/{subreddit.name}</a
+                            >
+                        </li>
                     {/each}
                     <li>
-                        <div>
+                        <div class="flex flex-row">
                             <input
                                 id="new-subreddit"
+                                class="input input-sm input-primary"
                                 type="text"
                                 placeholder="Add subreddit"
                                 bind:value={newSub}

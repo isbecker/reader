e984c2480220ddc5203c415fda7bf20ccfd799de
Ian Becker <ian@beckr.dev>
2024-01-19 20:33:46 -0500

    feat(reddit): tests comments section

---
+++ src/routes/r/[name]/comments/[id]/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import type Comment from "../../../../../types/reddit/Comment";
+  export let data;
+  let originalPost: Comment = data.op;
+  let comments: Comment[] = data.comments;
+</script>
+
+<div>{originalPost.permalink}</div>
+
+<div>
+  {#if originalPost.body}
+    <article class="prose">
+      {@html originalPost.body}
+    </article>
+  {/if}
+
+  <ul>
+    {#each comments as comment}
+      <li>
+        <article class="prose">
+          {@html comment.body}
+        </article>
+      </li>
+    {/each}
+  </ul>
+</div>

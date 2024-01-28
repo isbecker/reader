b7553cccd1cd72d9a73b26ada31612b2740be4ea
Ian Becker <ian@beckr.dev>
2024-01-27 20:14:05 -0500

    fix(reddit): not sure what this commit is

--- src/routes/r/[name]/+page.svelte
+++ src/routes/r/[name]/+page.svelte
@@ -2,16 +2,17 @@
   import PostCard from "../../../components/PostCard.svelte";
 
   export let data;
+
 </script>
 
 {#if data.subreddit}
-<ul>
-  {#each data.subreddit.posts as post}
-  <li class="hover:bg-base-300">
-    <div>
-      <PostCard {post} />
-    </div>
-  </li>
-  {/each}
-</ul>
+  <ul>
+    {#each data.subreddit.posts as post}
+      <li class="hover:bg-base-300">
+        <div>
+          <PostCard {post} />
+        </div>
+      </li>
+    {/each}
+  </ul>
 {/if}

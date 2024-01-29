feda9d2460b79f0fa022c36eeb16db26b6384ee0
Ian Becker <ian@beckr.dev>
2024-01-28 23:16:54 -0500

    feat(ux): adds scroll to top button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import "../app.css";
   import Navbar from "../components/Navbar.svelte";
   import Sidebar from "../components/Sidebar.svelte";
@@ -30,20 +30,21 @@
   ];
   let theme = "dark";
   let title = "";
+  let y = 0;
 </script>
+
+<svelte:window bind:scrollY={y} />
 
 <main class="bg-base-100">
   <div class="drawer">
     <input id="side-drawer" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content flex flex-col">
+    <div class="drawer-content h-full w-full">
       <!-- Navbar -->
       <div class="sticky top-0 z-50 bg-secondary">
         <Navbar {themes} {title} />
       </div>
 
-      <div class="">
-        <slot />
-      </div>
+      <slot />
     </div>
 
     <div class="drawer-side z-50">
@@ -53,5 +54,25 @@
         <Sidebar />
       </div>
     </div>
+  </div>
+
+  <div class="fixed bottom-0 right-0">
+    <button
+      class="btn btn-circle btn-base-300 hover:btn-secondary"
+      on:click={() => {
+        y = 0;
+      }}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        height="24"
+        viewBox="0 -960 960 960"
+        width="24"
+        class="fill-accent"
+        ><path
+          d="M440-160v-487L216-423l-56-57 320-320 320 320-56 57-224-224v487h-80Z"
+        /></svg
+      >
+    </button>
   </div>
 </main>

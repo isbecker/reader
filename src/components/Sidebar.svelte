bd71154beb2e45342de8ab540f1f45be6c93d9c2
Ian Becker <ian@beckr.dev>
2024-01-30 23:01:50 -0500

    feat(reddit): adds delete button for subs

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
     import { subscriptions } from "../stores/reddit/subscribed";
+    import type Subscription from "../types/reddit/Subscription";
 
     const dispatch = createEventDispatcher();
     let newSub: string = "";
@@ -75,9 +76,30 @@
                 <ul>
                     {#each $subscriptions as subreddit}
                         <li>
-                            <a class="btn btn-ghost" href={subreddit.url}
-                                >r/{subreddit.name}</a
-                            >
+                            <div class="flex flex-row">
+                                <a
+                                    class="btn btn-ghost grow"
+                                    href={subreddit.url}>r/{subreddit.name}</a
+                                >
+                                <button
+                                    class="btn btn-ghost"
+                                    on:click={() => {
+                                        $subscriptions = $subscriptions.filter(
+                                            (s) => s.name !== subreddit.name,
+                                        );
+                                    }}
+                                    ><svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        height="24"
+                                        viewBox="0 -960 960 960"
+                                        width="24"
+                                        class="fill-warning"
+                                        ><path
+                                            d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"
+                                        /></svg
+                                    ></button
+                                >
+                            </div>
                         </li>
                     {/each}
                     <li>

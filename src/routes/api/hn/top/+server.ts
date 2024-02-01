42907b9507e072f63c890298a29f04668879ba91
Ian Becker <ian@beckr.dev>
2024-01-31 21:40:54 -0500

    feat(hn): tries to add caching headers

--- src/routes/api/hn/top/+server.ts
+++ src/routes/api/hn/top/+server.ts
@@ -14,7 +14,10 @@ export const GET: RequestHandler = async () => {
   return new Response(JSON.stringify(hnJson), {
     status: 200,
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
+      'Cache-Control': 'max-age=10, public, s-maxage=10, stale-while-revalidate=10, stale-if-error=10',
+      'CDN-Cache-Control': 'max-age=60, public, s-maxage=60, stale-while-revalidate=60, stale-if-error=60',
+      'Vercel-CDN-Cache-Control': 'max-age=3600, public, s-maxage=3600, stale-while-revalidate=3600, stale-if-error=3600',
     }
   });
 }

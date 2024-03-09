e70a9f0abc3740861495272302e9ac3dacd6e4bc
Ian Becker <ian@beckr.dev>
2024-03-08 22:44:54 -0500

    fix(auth): sets jwt and refresh separately
    ---
    
    jwt cookie expires rapidly, should not hinder setting refresh, which is longer lived

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,13 +3,29 @@ import { parse } from 'cookie';
 
 export const handle: Handle = async ({ event, resolve }) => {
 
+  if (import.meta.env.DEV) {
+    const mockJwt = import.meta.env.MOCK_JWT;
+    const mockRefresh = import.meta.env.MOCK_REFRESH_TOKEN;
+
+    event.locals.user = {
+      jwt: mockJwt,
+      refresh: mockRefresh
+    };
+
+    return resolve(event);
+  }
+
+
   const httpCookie = parse(event.request.headers.get("Cookie") || "");
   const jwt = httpCookie["AccessToken"] ?? null;
   const refresh = httpCookie["RefreshToken"] ?? null;
 
 
-  if (jwt && refresh) {
-    event.locals.user = { jwt, refresh };
+  if (jwt) {
+    event.locals.user.jwt = jwt
+  }
+  if (refresh) {
+    event.locals.user.refresh = refresh
   }
 
   return resolve(event);
